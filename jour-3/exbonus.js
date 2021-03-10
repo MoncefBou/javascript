b43aac91657fc860f7965f4d3bd0bbf5cf2220ea
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-03-10 17:30:37 +0100

    exercice js jour-3

--- jour-3/exbonus.js
+++ jour-3/exbonus.js
@@ -1,14 +1,15 @@
-for ( i = 0 ; i <= 100, i++) {
-    if ( i % 3 == 0 && i % 5 == 0 ) {
-        console.log(i+ " est un multiple de 3 et 5 donc fizzbuzz")
-    } else if ( i % 3 == 0 ) {
-        console.log(i+ " est un multiple de 3 donc fizz")
-    } else if ( i % 5 == 0 ) {
-        console.log(i+ " est un multiple de 3 et 5 donc buzz")
-    } else if (i % 7 == 0 ) {
-        continue
+for (i = 0; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + " est un multiple de 3 et 5 donc fizzbuzz");
+    } else if (i % 3 == 0) {
+        console.log(i + " est un multiple de 3 donc fizz");
+    } else if (i % 5 == 0) {
+        console.log(i + " est un multiple de 3 et 5 donc buzz");
+    } else if (i % 7 == 0) {
+        continue;
     } else {
-        console.log(i)
+        console.log(i);
     }
 }
 
