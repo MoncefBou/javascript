7f15ac3c1247fb5610b1a5a72e7dd56b523e85f6
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-03-10 16:40:35 +0100

    exercice jour-3 js en cours

--- jour-3/ex6.js
+++ jour-3/ex6.js
@@ -4,7 +4,9 @@ sentence = sentence.split("");
 for ( i = 14; i >= 0; i--) {
         sentence.push(sentence[i]);
 
-    
 }
+
+sentence = sentence.slice(15)
+sentence = sentence.join("");
 
 console.log(sentence);
