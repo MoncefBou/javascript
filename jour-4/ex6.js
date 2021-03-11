0fc5f13a63ed391bc3e7fb30c66ec41c69a1329f
HD M2S <hdm2s@MacBook-Pro-de-HD.local>
2021-03-11 15:52:47 +0100

    ex.js jour-4 en cours

--- jour-4/ex6.js
+++ jour-4/ex6.js
@@ -1,3 +1,14 @@
 function format(num) {
+    var withoutHours = num % 3600;
+    var hours = (num - withoutHours) / 3600;
     
+    var withoutMinutes = withoutHours % 60;
+    var minutes = (withoutHours - withoutMinutes) / 60;
+    
+    var secondes = withoutMinutes;
+
+    var result = hours + ":" + minutes + ":" + secondes;
+    return result;
 }
+
+console.log(format(5000));
