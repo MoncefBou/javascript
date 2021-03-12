307820b22c42dbf4adbf1e2bf4289e94c74a8902
HD M2S <hdm2s@macbook-pro-de-hd.home>
2021-03-12 14:13:24 +0100

    ex.js jour-5 en cours

--- jour-3/exbonus.js
+++ jour-3/exbonus.js
@@ -1,8 +1,5 @@
 for (i = 0; i <= 100; i++) {
-    if (i == 0) {
-        console.log(0);
-    }
-    else if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
         console.log(i + " est un multiple de 3 et 5 donc fizzbuzz");
     } else if (i % 3 == 0) {
         console.log(i + " est un multiple de 3 donc fizz");
