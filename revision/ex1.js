9372cbbc5304bedb72773c6e2c8c3d0ebcde9b16
HD M2S <hdm2s@MacBook-Pro-de-HD.local>
2021-03-15 15:58:42 +0100

    ex revision en cours

---
+++ revision/ex1.js
@@ -0,0 +1,9 @@
+function sortLetters (myString){
+    var letters = myString.split("")
+      
+      letters = letters.sort()
+      
+      letters = letters.join("")
+      console.log(letters)
+ }
+ sortLetters("konexio")
